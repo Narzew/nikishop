05f47aa492e36e25fb0f973215b9caed7a74b449
Nikodem Solarz <narzew@gmail.com>
2021-10-30 21:44:14 +0200

    List with pagination - first try

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,5 +23,11 @@ class Product < ApplicationRecord
 
   # == Class Methods ========================================================
 
+  def self.paged(page:, per_page:)
+    page_number = !page.nil? && page.to_i >= 1 ? page.to_i : DEFAULT_PAGE
+    per_page_count = !per_page.nil? && per_page.to_i >= 1 ? per_page.to_i : DEFAULT_PER_PAGE
+    page(page_number).per(per_page_count)
+  end
+
   # == Instance Methods =====================================================
 end
