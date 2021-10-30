60438ec1f8ae12b7c6ae1a6a8a69c2feff33819f
Nikodem Solarz <narzew@gmail.com>
2021-10-30 22:13:21 +0200

    Pagination fixes

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,4 +2,14 @@
 
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :seq
+  UNKNOWN_VALUE = "".freeze
+
+  def name
+    return UNKNOWN_VALUE unless object.name
+    object.name
+  end
+
+  def seq
+    return 0 unless object.seq
+  end
 end
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,4 +2,25 @@
 
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :price, :category
+  UNKNOWN_VALUE = "".freeze
+
+  def name
+    return UNKNOWN_VALUE unless object.name
+    object.name
+  end
+
+  def description
+    return UNKNOWN_VALUE unless object.description
+    object.description
+  end
+
+  def price
+    return UNKNOWN_VALUE unless object.price
+    object.price
+  end
+
+  def category
+    return UNKNOWN_VALUE unless object.category.name
+    object.category.name
+  end
 end
