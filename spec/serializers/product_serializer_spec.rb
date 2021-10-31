625754874ae0793827582405f957c2d7699dd145
Narzew <narzew@gmail.com>
2021-10-31 21:34:29 +0100

    06 - Add basket (#7)
    * Cart system working
    
    * Rubocop job
    
    * Serializer specs
    
    * Add serializer specs
    
    * Rubocop job
    
    * Rubocop job

---
+++ spec/serializers/product_serializer_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ProductSerializer do
+  subject(:serialized_product) { described_class.new(product).to_h }
+  let(:category) { create(:category) }
+  let(:product) { create(:product, category: category) }
+
+  it { is_expected.to include(name: product.name) }
+  it { is_expected.to include(description: product.description) }
+  it { is_expected.to include(price: product.price) }
+  it { is_expected.to include(category: Category.find(product.category_id)['name']) }
+
+  describe 'name' do
+    context 'with name blank' do
+      let(:product) { build(:product, name: '') }
+      it { expect { product.save! }.to raise_error(ActiveRecord::RecordInvalid) }
+    end
+
+    context 'with name present' do
+      let(:product) { create(:product) }
+      it { is_expected.to include(name: product.name) }
+    end
+  end
+
+  describe 'description' do
+    context 'with description blank' do
+      let(:product) { create(:product, description: '') }
+      it { is_expected.to include(description: ProductSerializer::UNKNOWN_VALUE) }
+    end
+
+    context 'with description present' do
+      let(:product) { create(:product) }
+      it { is_expected.to include(description: product.description) }
+    end
+  end
+
+  describe 'price' do
+    context 'with price blank' do
+      let(:product) { create(:product, price: -1) }
+      it { expect { product.save! }.to raise_error(ActiveRecord::RecordInvalid) }
+    end
+
+    context 'with price present' do
+      let(:product) { create(:product) }
+      it { is_expected.to include(price: product.price) }
+    end
+  end
+
+  describe 'category' do
+    context 'with category blank' do
+      let(:product) { create(:product, category: nil) }
+      it { expect { product.save! }.to raise_error(ActiveRecord::RecordInvalid) }
+    end
+
+    context 'with category present' do
+      let(:product) { create(:product) }
+      it { is_expected.to include(category: Category.find(product.category_id)['name']) }
+    end
+  end
+end
