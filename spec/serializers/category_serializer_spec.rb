6146b66ac4d0617fe305b3c5db1d0aea322036cf
Nikodem Solarz <narzew@gmail.com>
2021-10-31 21:26:42 +0100

    Rubocop job

--- spec/serializers/category_serializer_spec.rb
+++ spec/serializers/category_serializer_spec.rb
@@ -1,28 +1,29 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe CategorySerializer do
-    subject(:serialized_category) { described_class.new(category).to_h }
-    let(:category) { create(:category) }
+  subject(:serialized_category) { described_class.new(category).to_h }
+  let(:category) { create(:category) }
 
-    it { is_expected.to include(name: category.name) }
+  it { is_expected.to include(name: category.name) }
 
-    describe 'name' do
-        context 'with name blank' do
-          let(:category) { build(:category, name: '') }
-          it { expect {category.save!}.to  raise_error(ActiveRecord::RecordInvalid) }
-        end
-    
-        context 'with name present' do
-            let(:category) { create(:category) }
-            it { is_expected.to include(name: category.name) }
-        end
+  describe 'name' do
+    context 'with name blank' do
+      let(:category) { build(:category, name: '') }
+      it { expect { category.save! }.to raise_error(ActiveRecord::RecordInvalid) }
     end
 
-    describe 'seq' do
-        context 'with seq blank' do
-          let(:category) { build(:category, seq: nil) }
-          it { is_expected.to include(seq: 0) }
-        end
+    context 'with name present' do
+      let(:category) { create(:category) }
+      it { is_expected.to include(name: category.name) }
     end
+  end
 
+  describe 'seq' do
+    context 'with seq blank' do
+      let(:category) { build(:category, seq: nil) }
+      it { is_expected.to include(seq: 0) }
+    end
+  end
 end
